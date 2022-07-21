5a656348defae5bffe05e16cdeac662f792e2601
BobbyDeluna <bobby.deluna2@gmail.com>
2022-07-21 12:39:58 -0700

    Completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import blogData from "../data/blog";
 import Header from "../components/Header";
+import About from "../components/About";
+import Article from "../components/Article"
+import ArticleList from "../components/ArticleList"
+import logo from "../assets/logo.svg";
 
 console.log(blogData);
 
@@ -9,7 +13,10 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
-      <Header/>
+    <Header name={blogData.name}/>
+    <About about = {blogData.about} image={blogData.image} logo={logo} />
+    <ArticleList posts={blogData.posts} id={blogData.id} />
+    <Article />
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 import React from "react";
-import blogData from "../data/blog";
 
-function Header(blogData){
+function Header(props){
     
-    return <div>
-        <h1>{blogData.name}</h1>
-        </div>
+    return <header>
+        <h1>{props.name}</h1>
+        </header>
 }
 
 export default Header;
